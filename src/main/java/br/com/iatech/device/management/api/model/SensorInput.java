00d59c5602d4947617f2dedc07759162ba56d97e
Israel Alencar <alenc4r25@gmail.com>
2025-08-13 17:27:31 -0400

    feat: starting the project development process

---
+++ src/main/java/br/com/iatech/device/management/api/model/SensorInput.java
@@ -0,0 +1,8 @@
+package br.com.iatech.device.management.api.model;
+
+public record SensorInput(String name,
+                          String ip,
+                          String location,
+                          String protocol,
+                          String model)
+{}
